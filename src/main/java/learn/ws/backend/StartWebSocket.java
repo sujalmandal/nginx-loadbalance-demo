5614db2e4ff272e9340a0d0e7ac1e28df7c6135f
Sujal Mandal <ss6sujal@gmail.com>
2024-04-11 01:29:46 +0400

    websocket lb and cors added for http lb

--- src/main/java/learn/ws/backend/StartWebSocket.java
+++ src/main/java/learn/ws/backend/StartWebSocket.java
@@ -1,6 +1,12 @@
 package learn.ws.backend;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.websocket.*;
 import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
@@ -8,6 +14,9 @@ import jakarta.websocket.server.ServerEndpoint;
 @ApplicationScoped
 @ServerEndpoint("/ws/{name}")
 public class StartWebSocket {
+
+    @Inject 
+    ObjectMapper objectMapper;
 
     @OnOpen
     public void onOpen(Session session, @PathParam("name") String name) {
@@ -25,8 +34,9 @@ public class StartWebSocket {
     }
 
     @OnMessage
-    public void onMessage(String message, @PathParam("name") String name) {
+    public String onMessage(String message, @PathParam("name") String name) throws JsonProcessingException {
         System.out.println("onMessage> " + name + ": " + message);
+        return objectMapper.writeValueAsString(Map.of("served-by", System.getenv("HOSTNAME"), "reply", message));
     }
 }
 
